81aacbbab8cc664cd5137bbf91a7418002ff42ff
MelanyCeleste <2509275@upy.edu.mx>
2026-06-12 10:06:57 -0600

    multiplicar los digitos e iniciar ciclo

--- CW07/CW07.py
+++ CW07/CW07.py
@@ -4,3 +4,11 @@
 rol = input("Ingrese el rol sin digito verificador: ")
 #Invertir el numero rol para facilitar el calculo del digito verificador
 rol_invertido = rol[::-1]
+#Multiplicar los digitos por la secuencia
+multiplicadores = [2, 3, 4, 5, 6, 7]
+suma = 0
+#iniciar ciclo para multiplicar cada digito por su multiplicador y sumar los resultados
+for i in range(len(rol_invertido)):
+    digito = int(rol_invertido[i])
+    multiplicador = multiplicadores[i % len(multiplicadores)]
+    suma += digito * multiplicador
